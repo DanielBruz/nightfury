0016f5a8da3d0ff3183eab2dd97f712ff2285bc3
DanielBruz <daniel.bruzenak@tiscali.cz>
2023-06-21 15:24:45 +0200

    Transfer to header.php

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Nightfury</title>
-
-	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600&amp;subset=latin,latin-ext">
-	<link rel="stylesheet" href="css/font-awesome.css">
-	<link rel="stylesheet" href="css/normalize.css">
-	<link rel="stylesheet" href="css/style.css">
-
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<script src="js/jquery-2.1.1.min.js"></script>
-	<!--[if lt IE 9]>
-		<script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<![endif]-->
-</head>
+<?php include 'header.php' ?>
 <body>
 
 	<header>
@@ -136,4 +119,3 @@
 	<script src="js/lightbox.js"></script>
 
 </body>
-</html>
